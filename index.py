ed88a50cc53650c3ea023a8801023053b8429167
otreblan <otreblain@gmail.com>
2021-07-24 19:56:12 -0500

    Add create_index()

--- index.py
+++ index.py
@@ -4,6 +4,7 @@ from face_recognition.api import load_image_file, face_encodings
 from glob import glob
 from sys import argv
 from typing import List
+from rtree.index import Index, Property
 
 import numpy as np
 
@@ -13,14 +14,28 @@ def get_files() -> List[str]:
 def point2box(v: np.ndarray) -> np.ndarray:
     return np.concatenate((v, v), axis=None)
 
+def create_index(name: str, dimensions: int, files: List[str]) -> Index:
+    p = Property()
+    p.dimension = dimensions
+    p.dat_extension = 'data'
+    p.idx_extension = 'index'
+
+    rtindex = Index(name, properties=p)
+
+    i = 0
+    for file in files:
+        for face in face_encodings(load_image_file(file)):
+            rtindex.insert(i, point2box(face))
+            # TODO i -> file map
+        i += 1
+
+    return rtindex
+
 def main() -> None:
     if(len(argv) < 2):
         exit(1)
 
-    for file in get_files():
-        for face in face_encodings(load_image_file(file)):
-            print(face)
-            # TODO
+    create_index('rtree', 128, get_files())
 
 if __name__ == "__main__":
     main()
