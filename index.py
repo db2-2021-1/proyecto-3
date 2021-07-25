3910f64f411c2608862572c40c26bbec97719113
otreblan <otreblain@gmail.com>
2021-07-24 19:30:52 -0500

    Add point2box

--- index.py
+++ index.py
@@ -5,18 +5,22 @@ from glob import glob
 from sys import argv
 from typing import List
 
+import numpy as np
+
 def get_files() -> List[str]:
     return glob(f'{argv[1]}/**/*.jpg', recursive=True)
+
+def point2box(v: np.ndarray) -> np.ndarray:
+    return np.concatenate((v, v), axis=None)
 
 def main() -> None:
     if(len(argv) < 2):
         exit(1)
 
     for file in get_files():
-        f = load_image_file(file)
-
-        # TODO
-        print(face_encodings(f))
+        for face in face_encodings(load_image_file(file)):
+            print(face)
+            # TODO
 
 if __name__ == "__main__":
     main()
